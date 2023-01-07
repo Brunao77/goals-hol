50abf9ce5b7a06ad54f7560dc83bbc49b4f8f8a6
Brunao77 <brucolon@gmail.com>
2023-01-06 23:44:58 -0300

    Change in _app

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -10,6 +10,7 @@ export default function App({ Component, pageProps }) {
           font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
             Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
             sans-serif;
+          background-color: #000;
         }
         * {
           box-sizing: border-box;
