a161983a9a18899e23c488e8cc738a3b12faf6ba
Brunao77 <brucolon@gmail.com>
2023-01-07 18:52:57 -0300

    Modified Anchor style

---
+++ client/components/Anchor.js
@@ -0,0 +1,27 @@
+import Link from "next/link";
+export const Anchor = ({ children, href, width, height }) => {
+  return (
+    <>
+      <Link href={href} legacyBehavior>
+        <a>{children}</a>
+      </Link>
+      <style jsx>{`
+        a {
+          border-radius: 25px;
+          border: 2px solid #fff;
+          background: transparent;
+          width: ${width};
+          height: ${height};
+          cursor: pointer;
+          font-weight: 600;
+          font-size: 20px;
+          color: #fff989;
+          text-decoration: none;
+          display: flex;
+          align-items: center;
+          justify-content: center;
+        }
+      `}</style>
+    </>
+  );
+};
