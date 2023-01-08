cf95b188be9e93f9214ddfa7b64af612b7edbe4e
Brunao77 <brucolon@gmail.com>
2023-01-08 15:36:17 -0300

    Change endpoints

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 import { Hono } from "hono";
 import { cors } from "hono/cors";
 import scorers from "../db/scorers.json";
+import historical_scorers_argentine from "../db/historical-scorers-argentine.json";
 
 const app = new Hono();
 app.use("*", cors());
@@ -8,12 +9,20 @@ app.use("*", cors());
 app.get("/", (ctx) => {
   return ctx.json([
     {
-      endpoint: "/scorers",
+      endpoint: "/scorers/all",
       description: "Return the top historical scorers",
+    },
+    {
+      endpoint: "/scorers/argentine-league",
+      description: "Return the historical scorer of the Argentine league",
     },
   ]);
 });
 
-app.get("/scorers", (ctx) => ctx.json(scorers));
+app.get("/scorers/all", (ctx) => ctx.json(scorers));
+
+app.get("/scorers/argentine-league", (ctx) =>
+  ctx.json(historical_scorers_argentine)
+);
 
 export default app;
