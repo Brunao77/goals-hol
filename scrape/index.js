2ac5064cbe4b8c52ec66e28f12f22a3724a8c6bf
Brunao77 <brucolon@gmail.com>
2023-01-08 15:40:03 -0300

    Scrape scorers argentine

--- scrape/index.js
+++ scrape/index.js
@@ -1,7 +1,78 @@
 import * as cheerio from "cheerio";
 import { writeDBFile } from "../db/index.js";
 
-const URL = { scorer: "https://es.wikipedia.org/wiki/Goleador_(f%C3%BAtbol)" };
+const URL = {
+  scorer:
+    "https://es.wikipedia.org/wiki/Anexo:M%C3%A1ximos_goleadores_de_la_Primera_Divisi%C3%B3n_de_Argentina",
+};
+
+const scrape = async (url) => {
+  const res = await fetch(url);
+  const html = await res.text();
+  return cheerio.load(html);
+};
+
+const getScorers = async () => {
+  const $ = await scrape(URL.scorer);
+
+  const $rows = $("table tbody tr");
+
+  const DATA_SELECTORS = {
+    name: { selector: 1, typeOf: "string" },
+    goals: { selector: 2, typeOf: "number" },
+    season: { selector: 5, typeOf: "string" },
+  };
+
+  const scorers = [];
+
+  const scorersSelectorEntries = Object.entries(DATA_SELECTORS);
+
+  $rows.each((index, el) => {
+    if (index !== 0) {
+      const scorersEntries = scorersSelectorEntries.map(
+        ([key, { selector, typeOf }]) => {
+          const rawValue = $(el).find("td").eq(selector).text();
+          const cleanedValue = rawValue.trim();
+
+          const value =
+            typeOf === "number" ? Number(cleanedValue) : cleanedValue;
+          return [key, value];
+        }
+      );
+
+      const scorersFromEntries = Object.fromEntries(scorersEntries);
+
+      scorers.push(scorersFromEntries);
+    }
+  });
+
+  return scorers;
+};
+
+const getImg = async (name) => {
+  const $ = await scrape(`https://es.wikipedia.org/wiki/${name}`);
+
+  const $img = $(
+    "#content #bodyContent #mw-content-text .mw-parser-output table.infobox tbody tr td.imagen a.image"
+  );
+
+  const imageURL = $img.find("img").attr("src");
+
+  return imageURL;
+};
+
+const scorers = await getScorers();
+
+const scorersWithImg = await Promise.all(
+  scorers.map(async (scorer) => {
+    const imgURL = await getImg(scorer.name);
+    return { ...scorer, imgURL };
+  })
+);
+
+await writeDBFile("historical_scorers_argentine", scorersWithImg);
+
+/*const URL = { scorer: "https://es.wikipedia.org/wiki/Goleador_(f%C3%BAtbol)" };
 
 const scrape = async (url) => {
   const res = await fetch(url);
@@ -67,4 +138,4 @@ const scorersWithImg = await Promise.all(
   })
 );
 
-await writeDBFile("scorers", scorersWithImg);
+// await writeDBFile("scorers", scorersWithImg);*/
