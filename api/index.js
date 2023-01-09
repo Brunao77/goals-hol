9b1a15921f1271c81046bb56aa614d6ca1e7cc51
Brunao77 <brucolon@gmail.com>
2023-01-09 19:26:43 -0300

    Add endpoint world cup

--- api/index.js
+++ api/index.js
@@ -5,6 +5,7 @@ import scorers_argentine_league from "../db/scorers_argentine_league.json";
 import scorers_boca_juniors from "../db/scorers_boca_juniors.json";
 import scorers_river_plate from "../db/scorers_river_plate.json";
 import scorers_argentine_national_team from "../db/scorers_argentine_national_team.json";
+import scorers_world_cup from "../db/scorers_world_cup.json";
 
 const app = new Hono();
 app.use("*", cors());
@@ -31,6 +32,10 @@ app.get("/", (ctx) => {
       endpoint: "/scorers/argentine_national_team",
       description: "Return the historical Argentine National Team scorers",
     },
+    {
+      endpoint: "/scorers/world_cup",
+      description: "Return the historical World Cup scorers",
+    },
   ]);
 });
 
@@ -47,5 +52,7 @@ app.get("/scorers/river_plate", (ctx) => ctx.json(scorers_river_plate));
 app.get("/scorers/argentine_national_team", (ctx) =>
   ctx.json(scorers_argentine_national_team)
 );
+
+app.get("/scorers/world_cup", (ctx) => ctx.json(scorers_world_cup));
 
 export default app;
