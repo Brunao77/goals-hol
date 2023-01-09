8336553ed11205cc6aeaa626af06e63c13ac8a01
Brunao77 <brucolon@gmail.com>
2023-01-09 19:02:59 -0300

    Add scrape scorers argentine national team

--- scrape/index.js
+++ scrape/index.js
@@ -8,6 +8,8 @@ const URL = {
   boca_juniors:
     "https://es.wikipedia.org/wiki/Anexo:M%C3%A1ximos_goleadores_del_Club_Atl%C3%A9tico_Boca_Juniors#:~:text=Los%20mayores%20goleadores%20del%20club,Sebasti%C3%A1n%20Villa%20con%2028%20tantos.",
   river_plate: "http://riverplaydigital.com/maximos-goleadores/",
+  argentine_national_team:
+    "https://es.wikipedia.org/wiki/Anexo:M%C3%A1ximos_goleadores_de_la_selecci%C3%B3n_de_f%C3%BAtbol_de_Argentina",
 };
 
 const scrape = async (url) => {
@@ -154,6 +156,45 @@ const getScorers = async (typeScorers) => {
       });
       break;
     }
+    case "argentine_national_team": {
+      const $rows = $("table tbody tr td table.sortable tbody tr");
+
+      const DATA_SELECTORS = {
+        name: { selector: 1, typeOf: "string" },
+        season: { selector: 2, typeOf: "string" },
+        goals: { selector: 3, typeOf: "number" },
+      };
+
+      const scorersSelectorEntries = Object.entries(DATA_SELECTORS);
+
+      $rows.each((index, el) => {
+        if (index !== 0) {
+          const scorersEntries = scorersSelectorEntries.map(
+            ([key, { selector, typeOf }]) => {
+              if (key === "goals") {
+                const rawValue = $(el).find("th").text();
+                const cleanedValue = rawValue.trim();
+
+                const value =
+                  typeOf === "number" ? Number(cleanedValue) : cleanedValue;
+                return [key, value];
+              }
+              const rawValue = $(el).find("td").eq(selector).text();
+              const cleanedValue = rawValue.trim();
+
+              const value =
+                typeOf === "number" ? Number(cleanedValue) : cleanedValue;
+              return [key, value];
+            }
+          );
+
+          const scorersFromEntries = Object.fromEntries(scorersEntries);
+
+          scorers.push(scorersFromEntries);
+        }
+      });
+      break;
+    }
   }
 
   return scorers;
@@ -171,8 +212,7 @@ const getImg = async (name) => {
   return imageURL;
 };
 
-const scorers = await getScorers("river_plate");
-console.log(scorers);
+const scorers = await getScorers("argentine_national_team");
 
 const scorersWithImg = await Promise.all(
   scorers.map(async (scorer) => {
@@ -181,6 +221,7 @@ const scorersWithImg = await Promise.all(
   })
 );
 
+// await writeDBFile("scorers_argentine_national_team", scorersWithImg);
 // await writeDBFile("scorers_river_plate", scorersWithImg);
 // await writeDBFile("scorers", scorersWithImg);
 // await writeDBFile("scorers_boca_juniors", scorersWithImg);
